f920f0cf9a8e4ec6159ae9e93e6320f87c47bf0d
Peter Stakoun <peter@stakoun.com>
2017-02-11 21:51:36 -0500

    Add server and public

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+var port = 3000;
+http.listen(port, function() {
+	    console.log('Server running on port ' + port);
+});
