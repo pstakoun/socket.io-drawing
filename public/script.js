7444a04f38d7a96e7f79687051df0a2deac1c302
Peter Stakoun <peter@stakoun.com>
2017-02-15 18:53:06 -0500

    Add client JS

---
+++ public/script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+
+	canvas.width = width;
+	canvas.height = height;
+
+	var drawing = false;
+	var x, y, prevX, prevY;
+
+	var socket = io.connect();
+
+	canvas.onmousedown = function(e) {
+		drawing = true;
+		prevX = x;
+		prevY = y;
+	}
+
+	canvas.onmouseup = function(e) {
+		drawing = false;
+	}
+
+	canvas.onmousemove = function(e) {
+		x = e.clientX;
+		y = e.clientY;
+		if (drawing) {
+			socket.emit('draw', {
+				'x1': prevX,
+				'y1': prevY,
+				'x2': x,
+				'y2': y
+			});
+
+			drawLine(prevX, prevY, x, y);
+			prevX = x;
+			prevY = y;
+		}
+	}
+
+	socket.on('draw', function(data) {
+		drawLine(data.x1, data.y1, data.x2, data.y2);
+	});
+
+	function drawLine(x1, y1, x2, y2) {
+		context.moveTo(x1, y1);
+		context.lineTo(x2, y2);
+		context.stroke();
+	}
+});
